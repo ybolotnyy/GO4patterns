1e7d9cee21d5e21b31b7de8c4da03e81fb9ac1ea
ybolotnyy <ybolotnyy@google.com>
2016-07-21 21:11:10 -0700

    let's implement factory pattern

---
+++ src/g04patterns/FactoryPattern/Main.java
@@ -0,0 +1,14 @@
+package g04patterns.FactoryPattern;
+
+import g04patterns.DecoratorPattern.Computer;
+import g04patterns.DecoratorPattern.DecoratorComponents.*;
+
+/**
+ * Created by byn on 7/21/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
