bb79c46d59a6ea9ee2e3e4d4bea5e515dc4be82a
ybolotnyy <ybolotnyy@google.com>
2016-07-21 18:51:06 -0700

    basic func-ty works

--- src/g04patterns/StrategyPattern/Vehicle.java
+++ src/g04patterns/StrategyPattern/Vehicle.java
@@ -4,8 +4,10 @@ package g04patterns.StrategyPattern;
  * Created by byn on 7/21/16.
  */
 public class Vehicle {
+    private String className;
 
     public void go() {
-        System.out.println("I'm going now");
+        className = getClass().getName().substring(28);
+        System.out.printf("I'm (%s) going now \n", className);
     }
 }
