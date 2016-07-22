60f3930de2a4202d12140ed686c6991ab004cb36
ybolotnyy <ybolotnyy@google.com>
2016-07-21 20:29:56 -0700

    started

---
+++ src/g04patterns/DecoratorPattern/Computer.java
@@ -0,0 +1,11 @@
+package g04patterns.DecoratorPattern;
+
+/**
+ * Created by byn on 7/21/16.
+ */
+public class Computer {
+
+    public String description() {
+        return "This computer has: ";
+    }
+}
---
+++ src/g04patterns/DecoratorPattern/Test.java
@@ -0,0 +1,14 @@
+package g04patterns.DecoratorPattern;
+
+/**
+ * Created by byn on 7/21/16.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Computer computer = new Computer();
+
+        System.out.println(computer.description());
+    }
+}
