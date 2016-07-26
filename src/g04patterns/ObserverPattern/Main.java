e14b5378ce48d9777cbdbddc66b06c2dc36be1da
ybolotnyy <ybolotnyy@google.com>
2016-07-26 16:16:05 -0700

    observer branch started

---
+++ src/g04patterns/ObserverPattern/Main.java
@@ -0,0 +1,14 @@
+package g04patterns.ObserverPattern;
+
+import g04patterns.FactoryPatternGoFway.ConcreteFactory;
+import g04patterns.FactoryPatternGoFway.Connection;
+
+/**
+ * Created by byn on 7/21/16.
+ */
+public class Main {
+
+    public static void main(String[] args)
+    {
+    }
+}
