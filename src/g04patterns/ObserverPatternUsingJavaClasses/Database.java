800cd0dc1cee068cebf916ad4b67538b93fd9660
ybolotnyy <ybolotnyy@google.com>
2016-07-27 19:09:59 -0700

    observer v2 works

---
+++ src/g04patterns/ObserverPatternUsingJavaClasses/Database.java
@@ -0,0 +1,25 @@
+package g04patterns.ObserverPatternUsingJavaClasses;
+
+import g04patterns.ObserverPattern.Interfaces.Observer;
+
+import java.util.Observable;
+
+public class Database extends Observable {
+    public String operation;
+    public String record;
+
+    public void editRecord(String operation, String record) {
+        this.operation = operation;
+        this.record = record;
+        setChanged();
+        notifyObservers();
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public String getRecord() {
+        return record;
+    }
+}
