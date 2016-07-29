3a9827e40d2e3082827860bf798a0f17228daacf
ybolotnyy <ybolotnyy@google.com>
2016-07-28 22:05:20 -0700

    singleton works

---
+++ src/g04patterns/Singleton/Database.java
@@ -0,0 +1,32 @@
+package g04patterns.Singleton;
+
+import javax.xml.crypto.Data;
+
+public class Database {
+    private String name;
+    private int record;
+    private static Database singleInstance;
+
+    private Database(String n) {
+        name = n;
+        record = 0;
+    }
+
+    public static Database createInstance(String name) {
+        if (singleInstance == null) {
+            singleInstance = new Database(name);
+        }
+
+        return singleInstance;
+    }
+
+    public void editRecord(String operation) {
+        System.out.printf("Performing operation '$s' on the record '%d' \n",
+                operation,
+                record);
+    }
+
+    public String getName () {
+        return name;
+    }
+}
--- src/g04patterns/Singleton/Main.java
+++ src/g04patterns/Singleton/Main.java
@@ -1,8 +1,17 @@
 package g04patterns.Singleton;
 
+import javax.xml.crypto.Data;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Database database;
+
+        database = Database.createInstance("products");
+        System.out.printf("This is the '%s' database \n", database.getName());
+
+        database = Database.createInstance("customers");
+        System.out.printf("This is the '%s' database \n", database.getName());
     }
 }
