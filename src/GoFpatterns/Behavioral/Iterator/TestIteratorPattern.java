26544b33b86ab97de938f8c4fbc4ae7dee361eb8
ybolotnyy <ybolotnyy@google.com>
2016-08-04 11:45:54 -0700

    iterator works

--- src/GoFpatterns/Behavioral/Iterator/TestIteratorPattern.java
+++ src/GoFpatterns/Behavioral/Iterator/TestIteratorPattern.java
@@ -1,7 +1,26 @@
 package GoFpatterns.Behavioral.Iterator;
 
 public class TestIteratorPattern {
+
   public static void main(String[] args) {
-//    TestIteratorPattern t = new TestIteratorPattern();
+    TestIteratorPattern t = new TestIteratorPattern();
+  }
+
+  TestIteratorPattern(){
+    Division division = new Division("AlfaBeta");
+
+    division.addVP(new VP("Petr", "Customer Service"));
+    division.addVP(new VP("Vlad", "Customer Support"));
+    division.addVP(new VP("Marta", "Marketing"));
+    division.addVP(new VP("Olga", "Sales"));
+
+    DivisionIterator iterator = division.iterator();
+
+    iterator.next().print();
+    iterator.next().print();
+    iterator.next().print();
+    iterator.next().print();
   }
 }
+
+
